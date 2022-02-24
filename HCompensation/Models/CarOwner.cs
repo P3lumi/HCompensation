59ac859ef0631d74135f2c00ca2878d169e14138
Pelumi <toriolaabosede@gmail.com>
2022-02-24 11:46:35 +0100

    initial commit

---
+++ HCompensation/Models/CarOwner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCompensation.Models
+{
+    public class CarOwner
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
